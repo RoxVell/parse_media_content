73e09c6513f1a248e3df95caf28a730e86079c09
Anton <anton1.a@mail.ru>
2018-12-05 20:24:29 +0300

    change file structure

--- content.js
+++ content.js
@@ -1,46 +1,19 @@
 let list = document.querySelector('.panel')
 
-let createItem = (item) => {
-  // li > a > img + span
-  let panel = document.createElement('li')
-  let link = document.createElement('a')
-  let img = document.createElement('img')
-  let title = document.createElement('span')
-
-  // img.onerror = removeElement(this)
-
-  link.href = item
-  img.src = item
-  img.classList.add('item-preview')
-  title.innerText = item.slice(-3)
-
-  link.appendChild(img)
-  link.appendChild(title)
-  panel.appendChild(link)
-  list.appendChild(panel)
+let renderItems = (items) => {
+  
 }
 
-let updateBtn = document.querySelector('button.update')
+browser.runtime.onMessage.addListener((msg) => {
+  if (msg.message === 'parseResults') {
+    renderItems(msg.results)
+  }
+});
 
-updateBtn.addEventListener('click', () => {
-  let gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true })
+let gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true })
 
-  browser.tabs.executeScript(null, { file: "/content_scripts/index.js" })
-
-  gettingActiveTab.then((tabs) => {
-    browser.tabs.sendMessage(tabs[0].id, { name: 'Anton' })
-      .then(response => {
-        console.log(response)
-        removeAllChild(list)
-
-        response.forEach(item => {
-          createItem(item)
-        })
-      })
-      .catch(onError)
-  })
+gettingActiveTab.then(tabs => {
+  browser.tabs.sendMessage(tabs[0].id, { message: 'parsePage' })
 })
 
-let removeAllChild = (node) => {
-  while (node.firstChild) node.removeChild(node.firstChild)
-}
+browser.tabs.executeScript(null, { file: "./content_scripts/index.js" })
--- content_scripts/index.js
+++ content_scripts/index.js
@@ -1,8 +1,16 @@
-let parseFormats = ['mp4', 'jpg', 'png', 'mp3']
+const parseFormats = {
+  images: ['jpg', 'jpeg', 'png'],
+  videos: ['mp4'],
+  music: ['mp3']
+}
 
-function parse(request, sender, sendResponse) {
+function parse() {
   let body = document.body.innerHTML, results = []
-  
+
+  for (let format of Object.entries(parseFormats)) {
+    
+  }
+
   parseFormats.forEach(format => {
     let regExp = new RegExp(`(http|https):\/\/\\S*\.${format}`, 'gm')
     let res = body.match(regExp)
@@ -10,7 +18,16 @@ function parse(request, sender, sendResponse) {
     if (res) results.push(...res)
   })
 
-  sendResponse(results)
+  return results
 }
 
-browser.runtime.onMessage.addListener(parse)
+browser.runtime.onMessage.addListener(message => {
+  if (message.message === 'parsePage') {
+    let results = parse()
+
+    let sending = browser.runtime.sendMessage({
+      message: 'parseResults',
+      results
+    })
+  }
+})
