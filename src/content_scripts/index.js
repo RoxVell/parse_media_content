04c75478fd8044cf6b1a7840772ce8f6690e8a23
Anton <anton1.a@mail.ru>
2018-12-06 19:57:59 +0300

    Init npm & webpack project

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -4,15 +4,24 @@ const parseFormats = {
   music: ['mp3']
 }
 
-function parse() {
-  let body = document.body.innerHTML, results = []
+let parseByPerformance = () => {
+  if (!performance) return parseByRegexp()
 
-  for (let format of Object.entries(parseFormats)) {
+  performance.getEntries().forEach(item => {
+    if (item.initiatorType === 'img') {
+      
+    }
+  })
     
-  }
+}
 
+let getRegExpByFormat = format => new RegExp(`(http|https):\/\/\\S*\.${format}`, 'gm')
+
+let parseByRegexp = () => {
+  let body = document.body.innerHTML, results = []
+  
   parseFormats.forEach(format => {
-    let regExp = new RegExp(`(http|https):\/\/\\S*\.${format}`, 'gm')
+    let regExp = getRegExpByFormat(format)
     let res = body.match(regExp)
     
     if (res) results.push(...res)
